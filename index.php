bbc47e2caabec882e1bdf0e7011ad77266bb8347
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-07-27 18:47:01 -0500

    Agregamos lista de dias de la semana

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
 	<title>For real</title>
 </head>
 <body>
-	<h1>For Real</h1>
+	<h1>For Real</h1>-
     <p>This is for real resources.</p>
+    <ul>
+    	<li>lunes</li>
+    	<li>martes</li>
+    	<li>miercoles</li>
+    	<li>jueves</li>
+    	<li>viernes</li>
+    	<li>sabado</li>
+    	<li>domingo</li>
+    </ul>
 </body>
 </html>
